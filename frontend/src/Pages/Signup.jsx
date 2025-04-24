8b6b3465e33e1f21c7bac2f56ab40a46eebedb40
sumit <you@example.com>
2025-04-24 13:11:10 +0530

    same-page-scrolling

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -65,7 +65,6 @@ const Signup = () => {
             <option value="">Select Gender</option>
             <option value="Male">Male</option>
             <option value="Female">Female</option>
-            <option value="Other">Other</option>
           </select>
           <input
             type="tel"
